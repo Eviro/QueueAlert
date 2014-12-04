fc7aa188f994f1bf87fadfd9a612467e15532e89
Lasse G <lasse973@gmail.com>
2014-12-04 14:36:22 +0100

    first version

--- efectealert.py
+++ efectealert.py
@@ -1,66 +1,78 @@
 # -*- coding: utf-8 -*-
 import requests
 import xml.etree.ElementTree as ET
-from ticket import Ticket
 import time
 import os
-import sys
+
+supportPerson = "Lasse Gøransson"
+scTerm = "lasse"
+refreshRate = 20 #secs
+
+
+class Ticket:
+
+    def __init__(self):
+        self.headline = ""
+        self.supportPerson = ""
+        self.inc = ""
+        self.status = ""
+        self.servicecomment = ""
 
 
 
 def playSound():
-    os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 0.1, 1000))
-    os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 0.1, 1100))
-    os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 0.1, 1200))
-    os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 0.1, 1100))
-    os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 0.1, 1000))
-    os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 0.1, 900))
+   freg = 350
+   for x in xrange(2):
+        os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 0.1, freg))
+        freg += 300
 
+
+
+
+playSound()
 tickets = []
 
 while True:
-    prevNum = len(tickets)
-    # Get all cases from the servicedesk
+     prevNum = len(tickets)
+     # Get all cases from the servicedesk
 
-    r = requests.get('https://servicedesk.sdu.dk/efecte/search.ws?query=SELECT%20entity%20FROM%20entity%20WHERE%20referenceData.groupAttribute.code%20=%20%27support_group%27%20AND%20referenceData.target.id%20=%20%27506185%27%20AND%20template.id%20=%20%2736%27%20AND%20entity.hidden%20=%20false%20AND%20entity.deleted%20=%20false',auth=('servicevagt','servicevagt'))
+     r = requests.get('http://10.80.0.135/api/public/index.php/efecte/get/sd/506185')
 
-    root = ET.fromstring(r.text.encode('utf-8'))
+     root = ET.fromstring(r.text.encode('utf-8'))
 
-    numTickets = 0
-    tickets = []
-    for child in root:
-        save = False
-        newTicket = Ticket()
-        numTickets += 1
-        for attr in child:
+     numTickets = 0
+     tickets = []
+     for child in root:
+         save = False
+         newTicket = Ticket()
+         numTickets += 1
+         for attr in child:
 
-            if 'code' in attr.attrib:
-                if attr.attrib['code'] == "support_person":
-                    for value in attr:
-                        if value.attrib['name'] == "Lasse Gøransson".decode('utf-8'):
-                            newTicket.supportPerson = value.attrib['name']
-                            save = True
+             if 'code' in attr.attrib:
+                 if attr.attrib['code'] == "support_person":
+                     for value in attr:
+                         if value.attrib['name'] == supportPerson.decode('latin1'):
+                             newTicket.supportPerson = value.attrib['name']
+                             save = True
 
-                if attr.attrib['code'] == "service_comment":
-                    for value in attr:
-                        if "Lasse".lower() in value.text.lower():
-                            newTicket.servicecomment = value.text
-                            save = True
-
-
-                if attr.attrib['code'] == "subject":
-                    for value in attr:
-                        newTicket.headline = value.text
-
-                if attr.attrib['code'] == "status":
-                    for value in attr:
-                        newTicket.status = value.text
-
-                if attr.attrib['code'] == "efecte_id_copy":
-                    for value in attr:
-                        newTicket.inc = value.text
+                 if attr.attrib['code'] == "service_comment":
+                     for value in attr:
+                         if scTerm.lower() in value.text.lower():
+                             newTicket.servicecomment = value.text
+                             save = True
 
 
+                 if attr.attrib['code'] == "subject":
+                     for value in attr:
+                         newTicket.headline = value.text
+
+                 if attr.attrib['code'] == "status":
+                     for value in attr:
+                         newTicket.status = value.text
+
+                 if attr.attrib['code'] == "efecte_id_copy":
+                     for value in attr:
+                         newTicket.inc = value.text
 
 
 
@@ -68,14 +80,16 @@ while True:
 
 
 
-        if save:
-            tickets.append(newTicket)
-
-    if prevNum < len(tickets):
-        print '############## Ny sag #################'
-        playSound()
 
 
-    print "Previous: %s Now: %s" % (prevNum,len(tickets))
+         if save:
+             tickets.append(newTicket)
 
-    time.sleep(5)
+     if prevNum < len(tickets):
+         print '############## Ny sag #################'
+         playSound()
+
+
+     print "Previous: %s Now: %s at: %s" % (prevNum,len(tickets),str(time.strftime("%H:%M:%S")))
+
+     time.sleep(refreshRate)
